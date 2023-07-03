878f5eededb768ab978548050eda06722dea775b
PhatPhamCoder <phamhoangminhphat@gmail.com>
2023-07-03 09:23:30 +0700

    update url BE

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,6 +1,10 @@
 import socketIOClient from "socket.io-client";
-
+import { readFileSync } from "fs";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = "https://chatapp-be-4p01.onrender.com/";
 
-export const socket = socketIOClient.connect(URL);
+export const socket = socketIOClient.connect(URL, {
+  key: readFileSync("/path/to/client-key.pem"),
+  cert: readFileSync("/path/to/client-cert.pem"),
+  ca: [readFileSync("/path/to/server-cert.pem")],
+});
